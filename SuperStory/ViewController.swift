a547911f4725f9253b1b8850182e8558d23e5590
Faraz Fazli <farazfazli@gmail.com>
2016-09-08 12:29:28 -0400

    Initial Commit

---
+++ SuperStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperStory
+//
+//  Created by Faraz Fazli on 9/8/16.
+//  Copyright © 2016 Faraz Fazli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
