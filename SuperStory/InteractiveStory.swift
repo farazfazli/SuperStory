8340b2ac8f3888c48b5348eb5bb961901a9d4bc4
Faraz Fazli <farazfazli@gmail.com>
2016-09-12 12:33:23 -0400

    Refactor, animate text field

--- SuperStory/InteractiveStory.swift
+++ SuperStory/InteractiveStory.swift
@@ -9,16 +9,30 @@
 import Foundation
 import UIKit
 
-enum Story: String {
-    case ReturnTrip
+enum Story {
+    case ReturnTrip(String)
     case TouchDown
     case Homeward
-    case Rover
+    case Rover(String)
     case Cave
     case Crate
     case Monster
-    case Droid
+    case Droid(String)
     case Home
+    
+    var rawValue: String {
+        switch self {
+        case .ReturnTrip: return "ReturnTrip"
+        case .TouchDown: return "TouchDown"
+        case .Homeward: return "Homeward"
+        case .Rover: return "Rover"
+        case .Cave: return "Cave"
+        case .Crate: return "Crate"
+        case .Monster: return "Monster"
+        case .Droid: return "Droid"
+        case .Home: return "Home"
+        }
+    }
 }
 
 extension Story {
@@ -28,22 +42,22 @@ extension Story {
     
     var text: String {
         switch self {
-        case .ReturnTrip:
-            return "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. \"Help me, you're my only hope.\""
+        case .ReturnTrip(let name):
+            return "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. \"Help me, \(name) you're my only hope.\""
         case .TouchDown:
             return "You deftly land your ship near where the distress signal originated. You didn't notice anything strange on your fly-by, behind you is an abandoned rover from the early 21st century and a small crate."
         case .Homeward:
             return "You continue your course to Earth. Two days later, you receive a transmission from HQ saing that they have detected some sort of anomaly on the surface of Mars near an abandoned rover. They ask you to investigate, but ultimately the decision is yours because your mission has already run much longer than planned and supplies are low."
-        case .Rover:
-            return "The rover is covered in dust and most of the solar panels are broken. But you are quite surprised to see the on-board system booted up and running. In fact, there is a message on the screen. \"Come to 28.2342, -81.08273\". These coordinates aren't far but you don't know if your oxygen will last there and back."
+        case .Rover(let name):
+            return "The rover is covered in dust and most of the solar panels are broken. But you are quite surprised to see the on-board system booted up and running. In fact, there is a message on the screen. \"\(name), come to 28.2342, -81.08273\". These coordinates aren't far but you don't know if your oxygen will last there and back."
         case .Cave:
             return "Your EVA suit is equipped with a headlamp which you use to navigate to a cave. After searching for a while your oxygen levels are starting to get pretty low. You know you should go refill your tank, but there's a faint light up ahead."
         case .Crate:
             return "Unlike everything else around you the crate seems new and...alien. As you examine the create you notice something glinting on the ground beside it. Aha, a key! It must be for the crate..."
         case .Monster:
             return "You pick up the key and try to unlock the crate, but the key breaks off in the keyhole.You scream out in frustration! Your scream alerts a creature that captures you and takes you away..."
-        case .Droid:
-            return "After a long walk slightly uphill, you end up at the top of a small crater. You look around and are overjoyed to see your robot friend, Droid-S1124. It had been lost on a previous mission to Mars. You take it back to your ship and fly back to Earth."
+        case .Droid(let name):
+            return "After a long walk slightly uphill, you end up at the top of a small crater. You look around and are overjoyed to see your robot friend, \(name)-S1124. It had been lost on a previous mission to Mars. You take it back to your ship and fly back to Earth."
         case .Home:
             return "You arrive home on Earth. While your mission was a success, you forever wonder what was sending that signal. Perhaps a future mission will be able to investigate."
         }
@@ -80,11 +94,11 @@ extension Page {
 }
 
 struct Adventure {
-    static func story() -> Page {
-        let returnTrip = Page(story: .ReturnTrip)
+    static func story(name: String) -> Page {
+        let returnTrip = Page(story: .ReturnTrip(name))
         let touchdown = returnTrip.addChoice("Stop and Investigate", story: .TouchDown)
         let homeward = returnTrip.addChoice("Continue Home to Earth", story: .Homeward)
-        let rover = touchdown.addChoice("Explore the Rover", story: .Rover)
+        let rover = touchdown.addChoice("Explore the Rover", story: .Rover(name))
         let crate = touchdown.addChoice("Open the Crate", story: .Crate)
         
         homeward.addChoice("Head back to Mars", page: touchdown)
@@ -93,7 +107,7 @@ struct Adventure {
         let cave = rover.addChoice("Explore the Coordinates", story: .Cave)
         rover.addChoice("Return to Earth", page: home)
         
-        cave.addChoice("Continue towards faint light", story: .Droid)
+        cave.addChoice("Continue towards faint light", story: .Droid(name))
         cave.addChoice("Refill the ship and explore the rover", page: rover)
         
         crate.addChoice("Explore the Rover", page: rover)
--- SuperStory/ViewController.swift
+++ SuperStory/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    enum TextError: ErrorType {
+        case NoName
+    }
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
+    var originalHeight: CGFloat = 0
+    var height: CGFloat = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        self.nameTextField.delegate = self
+        self.originalHeight = textFieldBottomConstraint.constant
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +34,49 @@ class ViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" {
-            if let pageController = segue.destinationViewController as? PageController {
-                pageController.page = Adventure.story()
+            
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw TextError.NoName
+                    }
+                if let pageController = segue.destinationViewController as? PageController {
+                        pageController.page = Adventure.story(name)
+                    }
+                }
+            } catch TextError.NoName {
+                let alertController = UIAlertController(title: "Name not provided", message: "Provide a name to start your story!", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                alertController.addAction(action)
+                presentViewController(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error)")
             }
         }
     }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfoDictionary = notification.userInfo, keyboardValue = userInfoDictionary[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardValue.CGRectValue()
+            
+            UIView.animateWithDuration(0.8) {
+                self.height = keyboardFrame.height / 1.2
+                self.textFieldBottomConstraint.constant = self.height
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        UIView.animateWithDuration(0.8) {
+        self.textFieldBottomConstraint.constant = self.originalHeight
+            self.view.layoutIfNeeded()
+        }
+        self.view.endEditing(true)
+        return false
+    }
 
-
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
 }
-
